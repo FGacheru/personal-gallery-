41c33b117187f5c1a7e8509edb4d24a91757c830
FGacheru <francisgacheru2001@gmail.com>
2021-01-09 22:11:32 +0300

    raise string-date converter in except block in past_days_image function

--- gallery/views.py
+++ gallery/views.py
@@ -28,6 +28,10 @@ def convert_dates(dates):
 def past_days_image(request,past_date):
         # Converts data from the string Url
         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+        
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
 
     day = convert_dates(date)
     html = f'''
