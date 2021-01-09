91011db7dba298de7e402645b9a32f26ca0b2fb2
FGacheru <francisgacheru2001@gmail.com>
2021-01-09 22:01:13 +0300

    create regex pattern to match the dates

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url,include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls)
-    url('^today/$',views.image_of_day,name='newsToday')
+    url(r'^admin/', admin.site.urls),
+    url('^today/$',views.image_of_day,name='newsToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_image,name = 'pastNews')
 ]
