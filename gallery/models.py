63167afa3fb70eb8f165b33e84ef95158f7a2287
FGacheru <francisgacheru2001@gmail.com>
2021-01-10 12:14:23 +0300

    add category class on the models file

--- gallery/models.py
+++ gallery/models.py
@@ -37,3 +37,19 @@ class Image(models.Model):
     def __str__(self):
         return self.name 
     
+class Category(models.Model):
+    name = models.CharField(max_length=60)
+    
+    def save_category(self):
+        self.save()
+        
+    def delete_category(self):
+        self.delete()
+        
+    @classmethod
+    def update_category(cls, id, value):
+        cls.objects.filter(id=id).update(name = value)
+        
+    def __str__(self):
+        return self.name 
+    
