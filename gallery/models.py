8b6fa1c8629a16db890ccf9780147a7d3bd3730a
FGacheru <francisgacheru2001@gmail.com>
2021-01-10 12:15:20 +0300

    add location class on the models file

--- gallery/models.py
+++ gallery/models.py
@@ -53,3 +53,19 @@ class Category(models.Model):
     def __str__(self):
         return self.name 
     
+class Location(models.Model):
+    name = models.CharField(max_length=60)
+    
+    def save_location(self):
+        self.save()
+        
+    def delete_location(self):
+        self.delete()
+        
+    @classmethod
+    def update_location(cls, id, value):
+        cls.objects.filter(id=id).update(name = value)
+        
+    def __str__(self):
+        return self.name 
+    
