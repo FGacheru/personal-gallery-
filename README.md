42809176f619b861f30ee33fdca9e0d870831945
FGacheru <francisgacheru2001@gmail.com>
2021-01-11 17:41:05 +0300

    add README.md file

---
+++ README.md
@@ -0,0 +1,27 @@
+# FRANKY GALLERY
+## Author
+By Francis Gacheru.
+## Description
+* Franky Gallery is a website that stores your photo and protects it. The website allows you to add photos with their description and place taken in the admin Panel
+## Setup/Installation requirements
+1. Clone  the repository from github, https://github.com/FGacheru/personal-gallery-
+
+3. cd into the project folder and open in text editor. 4. install virtual environment then activate it
+4. Download the Dependencies
+4. Run python3 manage.py runserver to run the app
+
+## Behavior Driven Development (BDD)
+
+| Behaviuor | Input | Output |
+| --------------- | --------------- | --------------- |
+| Fade in/out | Hover on image | fade in/out  |
+| Search       | Most astonishing site, Nature, Football | Desplays images related to the search |
+
+## Technologies Used
+* PYTHON
+* DJANGO FRAMEWORK
+* CSS
+
+## License
+Licensed under the MIT license 
+Copyright (c) [2020] (Francis Gacheru)
--- images/settings.py
+++ images/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 from pathlib import Path
 import os
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
@@ -22,11 +25,37 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
+MODE=config("MODE", default="dev")
+if config('MODE')=="dev":
+   DATABASES = {
+       'default': {
+           'ENGINE': 'django.db.backends.postgresql_psycopg2',
+           'NAME': config('DB_NAME'),
+           'USER': config('DB_USER'),
+           'PASSWORD': config('DB_PASSWORD'),
+           'HOST': config('DB_HOST'),
+           'PORT': '',
+       }
+       
+   }
+# production
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8faq3qpgqml#%j(al0f!!+70d!h*rkno4saqhh-a!2o)oqyzqw'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -45,6 +74,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -131,9 +161,13 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
+django_heroku.settings(locals())
 
 
 # cloudinary.config( 
